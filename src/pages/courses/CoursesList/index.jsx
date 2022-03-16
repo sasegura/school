ca9a1cf94d65635235ce9f76273d8e1eac990da4
Isabel García <ighilarion.1990@gmail.com>
2022-03-16 23:17:22 +0100

    cambio de plantilla

--- src/pages/courses/CoursesList/index.jsx
+++
@@ -1,6 +0,0 @@
-import { courses } from '../../../utils/courses/courses';
-import Course from '../Course';
-
-export default function CoursesList() {
-	return courses.map((course) => <Course course={course} />);
-}
