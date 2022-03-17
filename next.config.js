77759e185e70a17cb554ce7ed01218682ea833fa
Sergio <ssegura@op2aim.io>
2022-03-17 23:15:50 +0100

    asd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 const withImages = require('next-images')
 // module.exports = withImages();
 
-module.exports = {assetPrefix: './', ...withImages()}
+module.exports = withImages({assetPrefix: './'})
