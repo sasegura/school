6499fe055151de625e4d8a635b38266960ce397c
vladimir <vladimir.segura89@gmail.com>
2022-07-28 13:15:52 -0400

    ImageGallery: Se muestra la galería en pantalla completa cuando se le da click sobre la imágen actual.

--- components/gallery-image.js
+++ components/gallery-image.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useRef} from "react";
 import { AboutOneData } from "@/data";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
 
 const ImageGalleries = () => {
   const { sectionContent, carrousel } = AboutOneData;
+
+  const galleryRef = useRef();
+
+  const onClick = () => {
+    galleryRef.current.fullScreen();
+  }
 
   const images = carrousel.map((image) => ({
     original: image,
@@ -18,7 +24,7 @@ const ImageGalleries = () => {
           <h2 className="sec_title white">{sectionContent.subTitleGallery}</h2>
           {/* <h4 className="sub_title color_aaa">{sectionContent.subTitle}</h4> */}
         </div>
-        <ImageGallery items={images}/>
+        <ImageGallery ref={galleryRef} onClick={onClick} items={images} />
       </div>
     </section>
   );
