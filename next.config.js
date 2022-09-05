09d16e35d74d27d8a9f390cc2566aeb682d99a19
Sergio <ssegura@op2aim.io>
2022-09-06 00:01:38 +0200

    config

--- next.config.js
+++ next.config.js
@@ -13,3 +13,18 @@ const withVideos = require('next-videos')
 
 module.exports = withVideos()
 
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/apply': { page: '/apply' },
+      '/contact': { page: '/contact' },
+      '/courses': { page: '/courses' },
+      '/service': { page: '/service' },
+    }
+  },
+}
