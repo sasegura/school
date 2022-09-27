00498b65586f157b94c551e589de20e10cc34675
sasegura <61276217+sasegura@users.noreply.github.com>
2022-09-27 19:32:45 +0200

    Revert "config"

--- next.config.js
+++ next.config.js
@@ -13,18 +13,3 @@ const withVideos = require('next-videos')
 
 module.exports = withVideos()
 
-module.exports = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/apply': { page: '/apply' },
-      '/contact': { page: '/contact' },
-      '/courses': { page: '/courses' },
-      '/service': { page: '/service' },
-    }
-  },
-}
