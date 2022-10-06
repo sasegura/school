cc1ba40ae97d85918926fdeae230b1ba97134a9c
Isabel García <ighilarion.1990@gmail.com>
2022-10-06 10:04:12 +0200

    tag manager

--- components/layout.js
+++ components/layout.js
@@ -80,6 +80,17 @@ const Layout = ({ PageTitle, children }) => {
           loadScript(function(){beTracker.t({hash:"70b0b6774357eac3e22ac543ae794c66"})});
           `}
       </Script>
+
+      {/* <Script>
+        {`(function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NQVLV6F');`}
+      </Script> */}
     </Fragment>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,13 @@ import "@/css/icons.css";
 import "@/css/preset.css";
 import "@/css/theme.css";
 import "@/css/responsive.css";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-NQVLV6F" });
+  }, []);
   return <Component {...pageProps} />;
 }
 
